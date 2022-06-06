00ac9959f4ef3b5572ee789584723259091ecf4f
Michał Durlak <durlak.m@wp.pl>
2022-06-06 14:26:35 +0200

    Initial commit
    Initial commit with basics files

---
+++ src/main/java/pl/michaldurlak/URLReputationChecker/UrlReputationCheckerApplication.java
@@ -0,0 +1,13 @@
+package pl.michaldurlak.URLReputationChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlReputationCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlReputationCheckerApplication.class, args);
+	}
+
+}
